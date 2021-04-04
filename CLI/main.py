e9af76a02b95de3c3f3e3f79f2f48a5e2682248b
AC-BOSS <amartya.dps@gmail.com>
2021-04-04 23:29:09 +0530

    first query

--- CLI/main.py
+++ CLI/main.py
@@ -18,3 +18,20 @@ def executeQuery(query):
         print(i)
 
 #executeQuery("SHOW TABLES;")
+
+def get_address_with_rent_city():
+    city = input("Enter city: ")
+    min_rent = input("Enter min range: ")
+    max_rent = input("Enter max range: ")
+
+    cursor = db.cursor(prepared = True)
+
+    query = "SELECT DISTINCT address FROM transaction t INNER JOIN property p on(t.p_id = p.id) INNER JOIN locality_property lp ON(lp.p_id = p.id) INNER JOIN locality l ON(lp.l_id = l_id) WHERE (t.rent BETWEEN %s AND %s) AND l.locality = %s;"
+    cursor.execute(query, (min_rent, max_rent, city))
+
+    output = cursor.fetchall()
+
+    for i in output:
+        print(i)
+    
+#get_address_with_rent_city()
