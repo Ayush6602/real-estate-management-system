934865717b43d03fd223623a9e0cf6271d8e55ed
Ayush Das <54628493+Ayush6602@users.noreply.github.com>
2021-04-06 12:40:05 +0530

    added mail ids

--- Scraping/dealer.py
+++ Scraping/dealer.py
@@ -13,6 +13,7 @@ print('get: https://www.99acres.com/real-estate-agents-in-guwahati-ffid')
 driver.get('https://www.99acres.com/real-estate-agents-in-guwahati-ffid')
 
 dealers = []
+mails = ['gmail', 'yahoo', 'outlook', 'iiitg']
 
 try:
     i = 2
@@ -24,15 +25,18 @@ try:
         name: str = driver.find_element_by_xpath(
             f'//*[@id="results"]/div/div[{i}]/div/table/tbody/tr[2]/td/div/div[5]').text
         name = name[(name.find(':') + 1):name.find('|')]
-        name = name.strip()
+        name = name.strip().replace(' ', '_').lower()
+        mail: str = name.replace(' ', '_') + ''.join([random.choice(
+            string.digits) for _ in range(4)]) + '@' + random.choice(mails) + '.com'
         phone_number: str = ''.join(
             [random.choice(string.digits) for _ in range(10)])
-        print(locality, name, property_link, phone_number, sep='\n')
+        print(locality, name, property_link, phone_number, mail, sep='\n')
         dealers.append({
             'name': name,
             'locality': locality,
             'property_link': property_link,
             'phone_number': phone_number,
+            'mail': mail
         })
         i += 1
 finally:
