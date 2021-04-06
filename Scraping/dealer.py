944cdd18d542fee14654c5595584aef6b2d4c45a
Ayush Das <54628493+Ayush6602@users.noreply.github.com>
2021-04-06 13:01:36 +0530

    added dealers txt

--- Scraping/dealer.py
+++ Scraping/dealer.py
@@ -26,20 +26,16 @@ try:
             f'//*[@id="results"]/div/div[{i}]/div/table/tbody/tr[2]/td/div/div[5]').text
         name = name[(name.find(':') + 1):name.find('|')]
         name = name.strip().replace(' ', '_').lower()
+        password = name
         mail: str = name.replace(' ', '_') + ''.join([random.choice(
             string.digits) for _ in range(4)]) + '@' + random.choice(mails) + '.com'
         phone_number: str = ''.join(
             [random.choice(string.digits) for _ in range(10)])
         print(locality, name, property_link, phone_number, mail, sep='\n')
-        dealers.append({
-            'name': name,
-            'locality': locality,
-            'property_link': property_link,
-            'phone_number': phone_number,
-            'mail': mail
-        })
+        dealers.append('(' + name + ',' + password + ',' +
+                       phone_number + ',' + mail + ')')
         i += 1
 finally:
-    with open('dealers.json', 'w') as dealers_json:
-        json.dump(dealers, dealers_json, indent=4)
+    with open('dealers.txt', 'w') as dealers_txt:
+        dealers_txt.write(',\n'.join(dealers) + ';')
     driver.quit()
