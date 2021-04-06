1c122e58a25fe081685dac0ef313f9a17cdf12a6
AC-BOSS <amartya.dps@gmail.com>
2021-04-07 01:56:35 +0530

    queries b, c, e

--- CLI/main.py
+++ CLI/main.py
@@ -19,6 +19,7 @@ def executeQuery(query):
 
 #executeQuery("SHOW TABLES;")
 
+#first query
 def get_address_with_rent_city():
     city = input("Enter city: ")
     min_rent = input("Enter min range: ")
@@ -33,5 +34,47 @@ def get_address_with_rent_city():
 
     for i in output:
         print(i)
-    
 #get_address_with_rent_city()
+
+# second query
+def get_home_with_bedroom_rent():
+    bedroom = input("Enter the minimum desired bedrooms : ")
+    rent = input("Enter the maximum desired rent : ")
+
+    cursor = db.cursor()
+    query = "SELECT * FROM property NATURAL JOIN description WHERE bedroom >= %s AND rent <= %s;"
+    cursor.execute(query, (bedroom, rent))
+
+    output = cursor.fetchall()
+
+    for i in output:
+        print(i)
+# get_home_with_bedroom_rent()
+
+#third query
+def get_agent_with_date_money():
+    year = input("Enter year: ")
+
+    cursor = db.cursor(prepared = True)
+
+    query = "with dealer_sale AS(SELECT SUM(price) s,dealer FROM transaction WHERE year(date) = %s GROUP BY dealer) SELECT name FROM dealer_sale JOIN dealer WHERE dealer_sale.dealer = dealer.username AND s=(SELECT MAX(s) FROM dealer_sale);"
+    cursor.execute(query, (year,))
+
+    output = cursor.fetchall()
+
+    for i in output:
+        print(i)
+#get_agent_with_date_money()
+
+#fifth query
+def get_most_expensive():
+    cursor = db.cursor()
+
+    query = "SELECT * FROM property WHERE price = (SELECT MAX(price) FROM property) OR rent = (SELECT MAX(rent) FROM property);"
+    cursor.execute(query)
+
+    output = cursor.fetchall()
+
+    for i in output:
+        print(i)
+# get_most_expensive()
