2f6a177683e92600e63716704e222f3b58b0ddfe
Ayush Das <54628493+Ayush6602@users.noreply.github.com>
2021-04-06 13:15:57 +0530

    Added name to dealers.txt file

--- Scraping/dealer.py
+++ Scraping/dealer.py
@@ -13,6 +13,7 @@ print('get: https://www.99acres.com/real-estate-agents-in-guwahati-ffid')
 driver.get('https://www.99acres.com/real-estate-agents-in-guwahati-ffid')
 
 dealers = []
+dealers_dicts = []
 mails = ['gmail', 'yahoo', 'outlook', 'iiitg']
 
 try:
@@ -25,17 +26,28 @@ try:
         name: str = driver.find_element_by_xpath(
             f'//*[@id="results"]/div/div[{i}]/div/table/tbody/tr[2]/td/div/div[5]').text
         name = name[(name.find(':') + 1):name.find('|')]
-        name = name.strip().replace(' ', '_').lower()
-        password = name
+        name = name.strip()
+        username = name.replace(' ', '_').lower()
+        password = username
         mail: str = name.replace(' ', '_') + ''.join([random.choice(
             string.digits) for _ in range(4)]) + '@' + random.choice(mails) + '.com'
         phone_number: str = ''.join(
             [random.choice(string.digits) for _ in range(10)])
         print(locality, name, property_link, phone_number, mail, sep='\n')
-        dealers.append('(' + '"' + name + '"' + ',' + '"' + password + '"' + ',' +
+        dealers.append('(' + '"' + username + '"' + ',' + '"' + password + '"' + ',' + '"' + name + '"' + ',' +
                        phone_number + ',' + '"' + mail + '"' + ')')
+        dealers_dicts.append({
+            'username': username,
+            'password': password,
+            'name': name,
+            'contact_number': phone_number,
+            'mail': mail,
+            'locality': locality
+        })
         i += 1
 finally:
     with open('dealers.txt', 'w') as dealers_txt:
         dealers_txt.write(',\n'.join(dealers) + ';')
+    with open('dealers.json', 'w') as dealers_json:
+        json.dump(dealers_dicts, dealers_json)
     driver.quit()
