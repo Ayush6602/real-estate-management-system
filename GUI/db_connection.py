c373ca867887c254011da47e22c25ed96fe95b8d
Ayush Das <54628493+Ayush6602@users.noreply.github.com>
2021-04-10 13:14:53 +0530

    added login ui

---
+++ GUI/db_connection.py
@@ -0,0 +1,34 @@
+from tkinter.constants import SEL
+import mysql.connector as mysql
+from mysql.connector.cursor import MySQLCursor
+
+
+class DBConnection:
+
+    CLIENT = 'client'
+    ADMIN = 'admin'
+    DEALER = 'dealer'
+
+    def __init__(self) -> None:
+        self.connection = mysql.connect(
+            host="remotemysql.com",
+            user="AMLZ6ZziDM",
+            passwd="UQ5ebtcX8m",
+            database="AMLZ6ZziDM"
+        )
+        self.cursor: MySQLCursor = self.connection.cursor(prepared=True)
+
+    def get_user_type(self, username: str, password: str) -> str:
+        self.cursor.execute(
+            "select * from admin where username = %s and password = %s", (username, password))
+        if self.cursor.fetchone() is not None:
+            return 'admin'
+        self.cursor.execute(
+            "select * from dealer where username = %s and password = %s")
+        if self.cursor.fetchone() is not None:
+            return 'dealer'
+        self.cursor.execute(
+            "select * from client where username = %s and password = %s")
+        if self.cursor.fetchone() is not None:
+            return 'client'
+        return 'unknown user'
--- GUI/login_ui.py
+++ GUI/login_ui.py
@@ -1,3 +1,32 @@
-import tkinter
+from GUI.db_connection import DBConnection
+import tkinter as tk
 
-ui = tkinter.Tk()
+
+class LoginUi(tk.Frame):
+    def __init__(self, master, db_connection: DBConnection) -> None:
+        super().__init__(master)
+        self.master = master
+        self.username = tk.StringVar(value='Username')
+        self.password = tk.StringVar(value='Password')
+        self.db_connection = db_connection
+        tk.Entry(self, textvariable=self.username).pack()
+        tk.Entry(self, textvariable=self.password).pack()
+        tk.Button(self, text='Login', command=self.login).pack()
+        self.status_label = tk.Label(self, text='Enter Username and Password')
+        self.status_label.pack()
+
+    def login(self) -> None:
+        user_type = self.db_connection.get_user_type(
+            self.username.get(), self.password.get())
+        if user_type == DBConnection.ADMIN:
+            self.status_label.configure(
+                text='Welcome Admin', foreground='green')
+        elif user_type == DBConnection.DEALER:
+            self.status_label.configure(
+                text='Welcome Dealer', foreground='green')
+        elif user_type == DBConnection.CLIENT:
+            self.status_label.configure(
+                text='Welcome Client', foreground='green')
+        else:
+            self.status_label.configure(
+                text='Username Or Password Is Incorrect', foreground='red')
