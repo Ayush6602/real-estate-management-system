8419358f5c13a4e806734668f6857f21cb48e561
AC-BOSS <amartya.dps@gmail.com>
2021-04-11 16:49:30 +0530

    agent_ui

--- GUI/agent_ui.py
+++ GUI/agent_ui.py
@@ -1,33 +1,28 @@
+from GUI.db_connection import DBConnection
 import tkinter as tk
 
-window = tk.Tk()
-window.geometry("1000x500")
-window.title("Agent/Owner")
+class AgentUi(tk.Frame):
+    def __init__(self, master, db_connection: DBConnection, username) -> None:
+        super().__init__(master)
+        self.master = master
+        self.property_id = tk.StringVar(value="property")
+        self.db_connection = db_connection
+        self.username = username
 
-top_frame = tk.Frame(width=2000)
-heading = tk.Label(master = top_frame, text = "Agent/Owner").pack()
-top_frame.pack(fill = tk.X )
+        top_frame = tk.Frame(master = self)
+        top_frame.grid_columnconfigure([0,1], weight=1)
+        input_label = tk.Label(master = top_frame, text = "Enter Property ID:").grid(row=0, column=0)
+        property_entry = tk.Entry(top_frame).grid(row=0, column=1)
+        rent = tk.Button(master = top_frame, text="RENT").grid(row=1, column=0, pady=20)
+        sell = tk.Button(master = top_frame, text="SELL").grid(row=1, column=1, pady=20)
+        top_frame.pack(fill = tk.X, expand=True)
 
-main = tk.Frame(width=2000)
+        output_prop = tk.Frame(master = self, bg="yellow")
+        prop_lbl = tk.Label(master=output_prop, text = "Properties", bg="yellow").pack()
+        output_prop.pack(fill = tk.BOTH, side = tk.LEFT, expand = True)
 
-main.grid_columnconfigure(0, weight=1)
-main.grid_columnconfigure(1, weight=1)
+        output_trans = tk.Frame(master = self, bg="green")
+        trans_lbl = tk.Label(master=output_trans, text = "Transactions", bg="green").pack()
+        output_trans.pack(fill = tk.BOTH, side = tk.LEFT, expand = True)
 
-input_label = tk.Label(master = main, text = "Enter Property ID:")
-input_label.grid(row=0, column = 0, padx=20)
-
-property = tk.Entry(master=main).grid(row=0, column=1, padx=20)
-rent = tk.Button(master=main, text="RENT").grid(row=1, column=0, pady=20)
-sell = tk.Button(master=main, text="SELL").grid(row=1, column=1, pady=20)
-
-main.pack(fill = tk.X)
-
-output_prop = tk.Frame(width = 1000, bg="yellow")
-prop_lbl = tk.Label(master=output_prop, text = "Properties", bg="yellow").pack()
-output_prop.pack(fill = tk.BOTH, side = tk.LEFT, expand = True)
-
-output_trans = tk.Frame(width = 1000, bg="green")
-trans_lbl = tk.Label(master=output_trans, text = "Transactions", bg="green").pack()
-output_trans.pack(fill = tk.BOTH, side = tk.LEFT, expand = True)
-
-window.mainloop()
+    # def enterRent(self) -> None:
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from GUI.login_ui import LoginUi
+from GUI.agent_ui import AgentUi
 from GUI.db_connection import DBConnection
 import tkinter as tk
 
@@ -10,7 +11,9 @@ def main() -> None:
     root.geometry('450x450')
     app = LoginUi(root, db_connection)
     app.pack()
-    app.mainloop()
+    agent = AgentUi(root, db_connection, "guwahati_housing")
+    agent.pack(fill = tk.X)
+    root.mainloop()
 
 
 if __name__ == '__main__':
