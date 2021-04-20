f86e894b46ea555392615c9ce60df07888b21c47
almas33 <ahmadalmas.786.aa@gmail.com>
2021-04-20 22:29:02 +0530

    modified textbox admin_ui

--- GUI/admin_ui.py
+++ GUI/admin_ui.py
@@ -11,9 +11,10 @@ class AdminUi(tk.Canvas):
         self.master = master
         self.command_var = tk.StringVar(value='SQL Command')
         self.db_connection = db_connection
-        self.admin_bg_img = Image.open('C:\\Users\\Almas\\Documents\\Visual Studio 2019\\Code\\real-estate-management-system\\images\\admin_bg.jpg')
+        self.admin_bg_img = Image.open('images/admin_bg.jpg')
         self.admin_bg_pimg = PhotoImage(self.admin_bg_img)
         self.title_text = 'ADMIN'
+        self.command_text = None
         self.pack(expand=True, fill='both')
         self.bind('<Configure>', self.render)
 
@@ -35,16 +36,14 @@ class AdminUi(tk.Canvas):
         head_txt_font = f'ariel {min(width, height) // 20} bold'
         self.create_text(head_txt_x, head_txt_y,
                          text=self.title_text, font=head_txt_font, fill='yellow')
-        # set command line entry
+        # set command line text box
         cmd_ent_font = f'ariel {min(width, height) // 40}'
         cmd_ent_x = width / 2
         cmd_ent_y = 2 * height / 5
-        command_entry = tk.Entry(
-            self, textvariable=self.command_var, font=cmd_ent_font)
-        command_entry.bind('<ButtonRelease-1>', self.clear_command_line_entry)
-        command_entry.bind('<Return>', self.submit)
+        self.command_text = tk.Text(
+            self,height=height//100,width=width//20,font=cmd_ent_font)
         self.create_window(int(cmd_ent_x), int(
-            cmd_ent_y), window=command_entry)
+            cmd_ent_y), window=self.command_text)
         # set submit button
         sbmt_btn_font = f'ariel {min(width, height) // 50}'
         sbmt_btn_x = width / 2
@@ -67,11 +66,6 @@ class AdminUi(tk.Canvas):
                                 font=sale_btn_font, command=self.sales_report, activebackground='orange')
         self.create_window(int(sale_btn_x), int(sale_btn_y), window=sales_btn)
 
-    def clear_command_line_entry(self, event: tk.Event) -> None:
-        if self.command_var.get() == 'SQL Command':
-            self.command_var.set('')
-            self.render()
-
     def sales_report(self) :
         self.result = self.db_connection.get_sales_report()
         self.print_result()
@@ -81,16 +75,18 @@ class AdminUi(tk.Canvas):
         self.print_result()
 
     def submit(self, event: tk.Event = None) -> None:
-        query = self.command_var.get()
+        query = self.command_text.get('1.0','end')
+        print(query)
         self.result = self.db_connection.command_result (query)
         self.print_result()
+        self.db_connection.connection.commit()
 
     def print_result(self) :
-        ResultUi = tk.Tk()
-        ResultUi.title('Result')
-        ResultUi.geometry('1280x720')
+        result_ui = tk.Toplevel()
+        result_ui.title('Result')
+        result_ui.geometry('1280x720')
         res_font = f'courier'
-        tk.Label(ResultUi,text = self.result, font = res_font, fg='green', background='black').pack(fill='both', expand=1)
-        ResultUi.mainloop()
+        tk.Label(result_ui,text = self.result, font = res_font, fg='green', background='black').pack(fill='both', expand=1)
+        result_ui.mainloop()
         
         
